a144db6968d9463354c75521968515ba49187d73
kravandir <79300121+kravandir@users.noreply.github.com>
2022-03-04 23:40:51 +0400

    bspwm

---
+++ polybar/start.sh
@@ -0,0 +1,11 @@
+
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+polybar example 
